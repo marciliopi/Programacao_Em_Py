496d77c1805e5bc27db99a9c427284c75b4eb5a9
Marcilio <marciliopi@hotmail.com>
2024-09-21 17:41:32 -0300

    Update listagem4.6.py

--- else/listagem4.6.py
+++ else/listagem4.6.py
@@ -1,6 +1,7 @@
 minutos = int(input("Qauntos minutos você utilizou este mês: "))
 if minutos <= 200:
     preco = 0.20
+    print("Você vai pagar este mês: R$%6.2f" % (minutos * preco))
 else:
     if minutos >= 201 and minutos <= 400:
         preco = 0.18
