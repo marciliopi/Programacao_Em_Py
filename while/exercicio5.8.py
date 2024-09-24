f43c9152e8f49ea534f678815c7b0fac2ff6795d
Marcilio <marciliopi@hotmail.com>
2024-09-24 20:57:08 -0300

    Create exercicio5.8.py

---
+++ while/exercicio5.8.py
@@ -0,0 +1,11 @@
+'''Escreva um programa que leia dois números. Imprima o resultado da multiplicação do primeiro pelo segundo. Utilize apenas os operadores de soma e subtração para calcular o resultado. Lembre-se de que podemos entender a multiplicação de dois números como somas sucessivas de um deles.
+Assim, 4 x 5 = 5 + 5 + 5 + 5 = 4 + 4 + 4 + 4 + 4.'''
+
+p = int(input("Primeiro número: "))
+s = int(input("Segundo número: "))
+x = 1
+r = 0
+while x <= s:
+    r = r + p
+    x = x + 1
+print(f"{p} x {s} = {r}")
